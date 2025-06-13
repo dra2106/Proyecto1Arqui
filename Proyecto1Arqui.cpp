b11a0f36bba10267bf3de40e6416a2153f6f9ef8
Allan <allajox22@gmail.com>
2025-06-13 17:57:56 -0600

    Prototipo del movimiento de la nave
    La "nave" se mueve de derecha a izquierda, y se crea la caja donde se contienen los sprites.

--- Proyecto1Arqui.cpp
+++ Proyecto1Arqui.cpp
@@ -1,12 +1,34 @@
-// Proyecto1Arqui.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
 #include <iostream>
 #include <string>
+#include <ncurses.h>
 #include "Spaceship.h"
-int main()
-{
-    std::cout << "Hello World!\n";
-	return 0;
+int main() {
+    initscr();
+    cbreak();
+    noecho();
+
+    int height, width, start_y, start_x;
+    height = 30;
+    width = 50;
+    start_y = 0;
+    start_x = 30;
+
+    WINDOW* win = newwin(height, width, start_y, start_x);
+
+    box(win, 0, 0); // el primero es izq y der, el segundo arriba y abajo
+    refresh();
+    wrefresh(win); // refresca solo la ventana creada
+
+    // a height se le resta 2 para que salga arriba de la caja, y width se divide
+    // entre 2 para que salga en el centro
+    Spaceship* p = new Spaceship(win, height-2, width/2, '@');
+    do {
+        p->display();
+        wrefresh(win);
+    } while (p->getMove() != 'x'); // escribir x para salir
+
+    endwin();
+    return 0;
 }
 
 
--- Spaceship.h
+++ Spaceship.h
@@ -3,63 +3,109 @@
 #include <vector>
 class Spaceship {
 private:
-	int x_;
-	int y_;
-	int hearts;
-	bool inmune;
-	int cooldown;
-	std::list <std::string> sprite;
+	int xLoc, yLoc, xMax, yMax; // Loc es location
+	char character;
+	WINDOW* currentWindow; // la ventana donde se muestra la nave
+	//int hearts;
+	//bool inmune;
+	//int cooldown;
+	//std::list <std::string> sprite;
 public:
-	Spaceship() {
-		x_ = 0;
-		y_ = 0;
-		hearts = 3;
-		inmune = false;
-		cooldown = 0;
-		sprite = { "" };
+	Spaceship(WINDOW* win, int y, int x, char c) {
+		currentWindow = win;
+		yLoc = y;
+		xLoc = x;
+		getmaxyx(currentWindow, yMax, xMax);
+		keypad(currentWindow, true); // esto es para activar el uso de las flechas
+		character = c;
+		// x_ = 0;
+		// y_ = 0;
+		// hearts = 3;
+		// inmune = false;
+		// cooldown = 0;
+		// sprite = { "" };
 	}
 	~Spaceship() {
-		sprite.clear();
+		//sprite.clear();
 	}
 
-	void setX(int x) {
-		x_ = x;
+	void moveLeft() {
+		mvwaddch(currentWindow, yLoc, xLoc, ' '); // esto es para no dejar caracteres por donde se mueva
+		xLoc--;
+		// esta parte es para que no se salga de la caja
+		if (xLoc < 1)
+			xLoc = 1;
 	}
-	void setY(int y) {
-		y_ = y;
-	}
-	int getX(){
-		return x_;
-	}
-	int getY() {
-		return y_;
-	}
-	void setInmune(bool inm) {
-		inmune = inm;
-	}
-	void setCooldown(int cooldown) {
-		this->cooldown = cooldown;
-	}
-	void setHearts(int hearts) {
-		this->hearts = hearts;
-	}
-	bool isInmune() {
-		return inmune;
-	}
-	int getCooldown(){
-		return cooldown;
-	}
-	int getHearts() {
-	return hearts;
-	}
-	void move(char side) {
-	}
-	void receivedDamage() {
 
+	void moveRight() {
+		mvwaddch(currentWindow, yLoc, xLoc, ' ');
+		xLoc++;
+		// esta parte es para que no se salga de la caja
+		if (xLoc > xMax-2)
+			xLoc = xMax-2;
 	}
-	void update() {
 
+	int getMove() {
+		int choice = wgetch(currentWindow);
+		switch(choice) {
+			case KEY_UP: // por ahora, arriba no hace nada
+				break;
+			case KEY_DOWN: // abajo no hace nada
+				break;
+			case KEY_LEFT:
+				moveLeft();
+				break;
+			case KEY_RIGHT:
+				moveRight();
+				break;
+			default:
+				break;
+		}
+		return choice;
 	}
+
+	void display() {
+		mvwaddch(currentWindow, yLoc, xLoc, character);
+	}
+
+	// void setX(int x) {
+	// 	x_ = x;
+	// }
+	// void setY(int y) {
+	// 	y_ = y;
+	// }
+	// int getX(){
+	// 	return x_;
+	// }
+	// int getY() {
+	// 	return y_;
+	// }
+	// void setInmune(bool inm) {
+	// 	inmune = inm;
+	// }
+	// void setCooldown(int cooldown) {
+	// 	this->cooldown = cooldown;
+	// }
+	// void setHearts(int hearts) {
+	// 	this->hearts = hearts;
+	// }
+	// bool isInmune() {
+	// 	return inmune;
+	// }
+	// int getCooldown(){
+	// 	return cooldown;
+	// }
+	// int getHearts() {
+	// return hearts;
+	// }
+	// void move(char side) {
+	// }
+	// void receivedDamage() {
+
+	// }
+	// void update() {
+
+	// }
 	/*draw() {
 
 	}*/
