6cefa6d634df06ceb23b84706bda5c0923f03834
Josue Hidalgo <josuehidalgos2511@gmail.com>
2025-06-18 23:25:06 -0600

    Pantalla de Inicio y Scores

--- Proyecto1Arqui.cpp
+++ Proyecto1Arqui.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <ncurses.h>
 #include "Screen.h"
 #include "Spaceship.h"
 #include "MainLoop.h"
 #include "Entity.h"
+#include "ScoreController.h"
+#include "DataStructures/DLinkedList.h"
+#include "DataStructures/KVPair.h"
 
 using std::cout;
 using std::endl;
@@ -14,20 +18,71 @@ int main() {
     cbreak();
     noecho();
     curs_set(0);
-    nodelay(stdscr, TRUE); // <-- clave para que el juego corra solo
+    nodelay(stdscr, FALSE); // Espera por una tecla
     refresh();
+
+    const char* titulo[] = {
+        "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+        "@@@@@@@@@@@@@@@@@@@=:::::%@@@@@@@%:%@@@@@@@%:::::-:%@@@@@@@@@@@@@@@@@",
+        "@@@@@@@@@@@@@@@-::::::::::::@@@@-:::-@@@@::::::::::::::#@@@@@@@@@@@@@",
+        "@@@@@@@@@#**-::::::::::::::::=*:::::::*=::::::::::::::::::+#@@@@@@@@@",
+        "@@@@@@@@--::::::::::::::::::::-###:*##-::::::::::::::::::::--@@@@@@@@",
+        "@@@@@@+::::::::::-@=-@=:::::::-###:###-::::::@@@--@-::::::::::=@@@@@@",
+        "@@@@@:::::::-=@=*@@@@@+=@=-::::=======::::-=@@@@@@@=#@-%@--:::::#@@@@",
+        "@@@#=::::+*:%@@@@@@@@@@@@@@#-:-#######-::#@@@@@@@@@@@@@@@@%*:::::+@@@",
+        "@@@*::*%:#@@@@@@@@@@@@@@@@@@-:::#####:::-@@@@@@@@@@@@@@@@@@#:%#::+@@@",
+        "@@@*-@@@@@@@@@@@@@@@@@@@@@@@@+:::=#+:::+@@@@@@@@@@@@@@@@@@@@@@@@-+@@@",
+        "@@@%*@@@@@@@@@@@@@@@@@@@@@%+:-*@*+:+*@*-:+%@@@@@@@@@@@@@@@@@@@@@*%@@@",
+        "@@@@@@@@@@@@@@@@@@@@@@@@@::::+@@@@@@@@@*::::@@@@@@@@@@@@@@@@@@@@@@@@@",
+        "@@@@@@@@@@@@@@@@@@@@@@@@@:=@:+@@@@@@@@@*:@=:@@@@@@@@@@@@@@@@@@@@@@@@@",
+        "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+        "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+        "@@@@- =**+.+%  %@@- +#-.**+ =@: +****#% .=@@: #@%*+ :**@: %@@+ -@@@@@",
+        "@@@@- *@@@ :%  +**: +* :@@@  %: +***#@% .=.+: #@@@# :@@@@+.+::*@@@@@@",
+        "@@@@- ....:@%  %@@- +* :@@@  %: #@@@@@% .@@:  #@@@# :@@@@:.%-.=@@@@@@",
+        "@@@@- *@@@@@%  %@@- +@#.::: %@: .::::-% .@@@: #@*:. .::@: %@@+ -@@@@@",
+        "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    };
+    int num_lines = sizeof(titulo) / sizeof(titulo[0]);
+
     int rows, cols;
     getmaxyx(stdscr, rows, cols);
-    cols /= 3;
-    MainLoop game(rows - 2, cols - 4); // centrado con márgenes
+
+    clear();
+    for (int i = 0; i < num_lines; ++i) {
+        int len = strlen(titulo[i]);
+        int start_col = (cols - len) / 2;
+        int start_row = (rows - num_lines) / 2;
+        mvprintw(start_row + i, start_col, "%s", titulo[i]);
+    }
+    refresh();
+    getch(); // Espera a que el usuario presione cualquier tecla
+
+    clear();
+    refresh();
+    nodelay(stdscr, TRUE); // Vuelve a modo no bloqueante
+    getch();
+    clear();
+
+    MainLoop game(rows - 2, cols/3 - 4);
     while (!game.isOver()) {
         game.processInput();
         game.updateState();
         game.redraw();
-        napms(32); // ~30 FPS
+        napms(32);
     }
     getch();
     endwin();
     cout << "Has perdido!!" << endl;
+    // Mostrar highscores
+    ScoreController scoreController("scores.txt", "JUGADOR"); // Cambia el nombre si lo pides antes
+    DLinkedList<KVPair<std::string, int>> scores;
+    scoreController.getScores(scores);
+
+    cout << "\nHIGHSCORES:\n";
+    for (scores.goToStart(); !scores.atEnd(); scores.next()) {
+        auto pair = scores.getElement();
+        cout << pair.key << " - " << pair.value << " PTS" << endl;
+    }
     return 0;
 }
